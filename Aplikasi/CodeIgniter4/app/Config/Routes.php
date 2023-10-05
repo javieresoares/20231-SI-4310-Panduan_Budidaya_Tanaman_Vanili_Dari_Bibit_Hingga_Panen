1f073920262e6cd7ff2857103b1fd8abd5635e3e
javieresoares <113227657+javieresoares@users.noreply.github.com>
2023-10-05 17:42:51 +0700

    Update files via upload

--- Aplikasi/CodeIgniter4/app/Config/Routes.php
+++ Aplikasi/CodeIgniter4/app/Config/Routes.php
@@ -5,4 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('/register', 'AuthController::register');
+$routes->add('/register', 'AuthController::register'); 
+
+$routes->get('/login', 'AuthController::login');
+$routes->add('/login', 'AuthController::login'); 
+$routes->get('/logout', 'AuthController::logout');
