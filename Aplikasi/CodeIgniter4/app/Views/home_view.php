9cc6ba3a256de48af201be45c3fcc1796c4f7458
wahyutyas444 <113276395+wahyutyas444@users.noreply.github.com>
2023-10-05 19:08:43 +0700

    Update files via upload

--- Aplikasi/CodeIgniter4/app/Views/home_view.php
+++ Aplikasi/CodeIgniter4/app/Views/home_view.php
@@ -1,14 +1,18 @@
 <?= $this->extend('components/layout') ?>
 <?= $this->section('content') ?>
 
-<section class="section">
+<section class="section faq">
     <div class="row">
         <div class="col-lg-6">
 
         <div class="card">
             <div class="card-body">
-            <h5 class="card-title">Example Card</h5>
-            <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+            <h5 class="card-title">Selamat datang di VaniliMate !</h5>
+            <p>
+                Selamat datang di VaniliMate, teman petani vanili! <br>
+                <br>
+                Kami hadir untuk membantu Anda mengembangkan keahlian dalam budidaya tanaman vanili yang indah ini Dengan VaniliMate, Anda akan mendapatkan akses ke berbagai informasi, tips, dan sumber daya yang diperlukan untuk sukses dalam usaha budidaya vanili.
+            </p>
             </div>
         </div>
 
@@ -16,12 +20,34 @@
 
         <div class="col-lg-6">
 
-        <div class="card">
-            <div class="card-body">
-            <h5 class="card-title">Example Card</h5>
-            <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+        <div class="card basic">
+                <div class="card-body">
+                <h5 class="card-title">Fitur Utama :</h5>
+
+                <div>
+                    <h6>1. Panduan Budidaya Terperinci:</h6>
+                    <p>Aplikasi kami menyediakan panduan
+langkah demi langkah tentang cara
+menanam, merawat, dan memanen
+tanaman vanili dengan hasil terbaik.
+Mulai dari pemilihan bibit hingga teknik
+pemangkasan, kami mencakup semua
+aspek yang perlu Anda ketahui.
+</p>
+                </div>
+
+                <div class="pt-2">
+                    <h6>2. Pengingat Perawatan yang Tepat Waktu:</h6>
+                    <p>Kami mengerti bahwa perawatan vanili 
+memerlukan ketepatan waktu. Aplikasi ini
+memberikan pengingat untuk menyirami,
+memupuk, dan melindungi tanaman Anda
+dari hama penyakit. Anda tidak akan pernah
+melewatkan perawatan yang penting lagi.</p>
+                </div>
+
+                </div>
             </div>
-        </div>
 
         </div>
     </div>
